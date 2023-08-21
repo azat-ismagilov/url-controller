4d77dc64b4c7aba7465a4814ee9e350a920aa13b
Azat Ismagilov <azatismagilov00@gmail.com>
2023-08-21 22:10:54 +0200

    Update settings.gradle.kts with generator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-include(":frontend", ":backend")
+include(":frontend", ":backend", ":schema-generator")
 project(":frontend").projectDir = file("src/frontend")
 project(":backend").projectDir = file("src/backend")
+project(":schema-generator").projectDir = file("src/schema-generator")
