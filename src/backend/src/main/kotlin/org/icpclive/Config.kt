805940b4462ca5deb0ffe7791a1341a04d3bd984
Sergey Melnikov <sergey@neon.tech>
2024-03-05 21:56:58 +0100

    Configure basic auth for admin

--- src/backend/src/main/kotlin/org/icpclive/Config.kt
+++ src/backend/src/main/kotlin/org/icpclive/Config.kt
@@ -8,7 +8,17 @@ class Config(environment: ApplicationEnvironment) {
     private fun ApplicationConfig.stringOrNull(name: String) = propertyOrNull(name)?.getString()
     private fun ApplicationConfig.string(name: String) = property(name).getString()
     private fun ApplicationConfig.bool(name: String) = stringOrNull(name) == "true"
-    val authDisabled = environment.config.bool("auth.disabled")
+    val authEnabled = environment.config.bool("auth.enabled")
+    val authUsername = if (authEnabled) {
+        environment.config.string("auth.username")
+    } else {
+        null
+    }
+    val authPassword = if (authEnabled) {
+        environment.config.string("auth.password")
+    } else {
+        null
+    }
 }
 
 lateinit var config: Config
